83f4599b5919bd7687bfc512b3c81bf353adf00d
Paulo <paulo.leite@academico.ifpb.edu.br>
2021-03-11 14:50:26 -0300

    Parada Controller

---
+++ backend/src/controllers/ParadaController.js
@@ -0,0 +1,72 @@
+import Parada from '../models/Linha';
+
+class ParadaController {
+  async store(req, res) {
+    try {
+      const parada = await Parada.create(req.body);
+
+      return res.status(200).json(parada);
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const paradas = await Parada.findAll();
+
+      if (!paradas)
+        return res
+          .status(400)
+          .json({ msg: 'Não existem registros de paradas.' });
+
+      return res.status(200).json(paradas);
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.query;
+      const parada = await Parada.findByPk(id, {
+        attributes: ['id', 'name', 'latitude', 'longitude'],
+      });
+
+      if (!parada)
+        return res.status(400).json({ msg: 'Parada não encontrada' });
+
+      return res.status(200).json(parada);
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.query;
+      const parada = await Parada.findByPk(id);
+
+      const paradaAtualizada = await parada.update(req.body);
+
+      return res.status(200).json(paradaAtualizada);
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.query;
+      const parada = await Parada.findByPk(id);
+
+      await parada.destroy();
+
+      return res.status(200).json({ msg: 'Registro deletado com sucesso.' });
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+}
+
+export default new ParadaController();
--- backend/src/routes/parada.js
+++ backend/src/routes/parada.js
@@ -1,70 +1,16 @@
 import { Router } from 'express';
-import Parada from '../models/Parada';
+import ParadaController from '../controllers/ParadaController';
 
 const router = Router();
 
-router.post('/store', async (req, res) => {
-  try {
-    const parada = await Parada.create(req.body);
+router.post('/store', ParadaController.store);
 
-    return res.status(200).json(parada);
-  } catch (e) {
-    throw new Error(e);
-  }
-});
+router.get('/index', ParadaController.index);
 
-router.get('/index', async (req, res) => {
-  try {
-    const paradas = await Parada.findAll();
+router.get('/show', ParadaController.show);
 
-    if (!paradas)
-      return res.status(400).json({ msg: 'Não existem registros de paradas.' });
+router.put('/update', ParadaController.update);
 
-    return res.status(200).json(paradas);
-  } catch (e) {
-    throw new Error(e);
-  }
-});
-
-router.get('/show', async (req, res) => {
-  try {
-    const { id } = req.query;
-    const parada = await Parada.findByPk(id, {
-      attributes: ['id', 'name', 'latitude', 'longitude'],
-    });
-
-    if (!parada) return res.status(400).json({ msg: 'Parada não encontrada' });
-
-    return res.status(200).json(parada);
-  } catch (e) {
-    throw new Error(e);
-  }
-});
-
-router.put('/update', async (req, res) => {
-  try {
-    const { id } = req.query;
-    const parada = await Parada.findByPk(id);
-
-    const paradaAtualizada = await parada.update(req.body);
-
-    return res.status(200).json(paradaAtualizada);
-  } catch (e) {
-    throw new Error(e);
-  }
-});
-
-router.delete('/delete', async (req, res) => {
-  try {
-    const { id } = req.query;
-    const parada = await Parada.findByPk(id);
-
-    await parada.destroy();
-
-    return res.status(200).json({ msg: 'Registro deletado com sucesso.' });
-  } catch (e) {
-    throw new Error(e);
-  }
-});
+router.delete('/delete', ParadaController.delete);
 
 export default router;
