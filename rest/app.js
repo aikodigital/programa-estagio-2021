1d3eded04703d83bb2c903c6f3cd7f5cffdc8ecf
Heuller <heullergit@gmail.com>
2021-03-14 18:14:44 -0300

    primeiro crud realizado

---
+++ rest/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+const rotaProdutos = require('./routes/produtos');
+
+
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use('/produtos', rotaProdutos);
+
+app.use((req,res,next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro : {
+            mensagem: error.message
+        }
+    });
+});
+
+
+module.exports = app;
---
+++ rest/routes/produtos.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('../mysql').pool;
+
+//INSERE UMA LINHA
+router.post('/', (req,res,next) => {
+    mysql.getConnection(function (error, conn) {
+        if(error){ return res.status(500).send({error:error})}
+        conn.query(
+            'INSERT INTO linha (nome) VALUES (?)',
+            [req.body.nome],
+            (error, resultado, field) => {
+                conn.release();
+                if(error){
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    })
+                }
+                res.status(201).send({
+                    response: 'Linha inserida com sucesso.',
+                    id_linha: resultado.insertId
+                });
+            }
+        );
+    });
+});
+
+//RETORNA TODAS AS LINHAS
+router.get('/', (req,res,next) => {
+    mysql.getConnection((error,conn) => {
+        if(error){ return res.status(500).send({error:error})}
+        conn.query(
+            'SELECT * FROM linha;',
+            (error, resultado, fields) => {
+                if(error){ return res.status(500).send({error:error})}
+                return res.status(200).send({response: resultado});
+            }
+        );
+    })
+});
+
+//RETORNA UMA LINHA
+router.get('/:id_linha', (req,res,next) => {
+    mysql.getConnection((error,conn) => {
+        if(error){ return res.status(500).send({error:error})}
+        conn.query(
+            'SELECT * FROM linha WHERE idLinha= ?;',
+            [req.params.id_linha],
+            (error, resultado, fields) => {
+                if(error){ return res.status(500).send({error:error})}
+                return res.status(200).send({response: resultado});
+            }
+        );
+    })
+});
+
+
+//ATUALIZA UMA LINHA
+router.put('/', (req,res,next) => {
+    console.log(req.body);
+    mysql.getConnection((error,conn) => {
+        if(error){ return res.status(500).send({error:error})}
+        conn.query(
+            `UPDATE linha 
+                SET nome = ?
+             WHERE idLinha = ?`,
+            [req.body.nome, req.body.id_linha],
+            (error, resultado, fields) => {
+                if(error){ return res.status(500).send({error:error})}
+                return res.status(202).send({response: 'Linha atualizada com sucesso'});
+            }
+        );
+    })
+});
+
+//DELETA UMA LINHA
+router.delete('/', (req,res,next) => {
+    console.log(req.body);
+    mysql.getConnection((error,conn) => {
+        if(error){ return res.status(500).send({error:error})}
+        conn.query(
+            `DELETE FROM linha WHERE idLinha = ?`,
+            [req.body.id_linha],
+            (error, resultado, fields) => {
+                if(error){ return res.status(500).send({error:error})}
+                return res.status(202).send({response: 'Linha removida com sucesso'});
+            }
+        );
+    })
+});
+
+module.exports = router;
