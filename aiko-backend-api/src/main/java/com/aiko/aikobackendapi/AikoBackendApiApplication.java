0e9483422cce0ddbc5849634414efc20b8635380
Sadraque Nunes <sadraquenunesmartiniano@gmail.com>
2021-03-18 19:50:29 -0300

    inicio do projeto

---
+++ aiko-backend-api/src/main/java/com/aiko/aikobackendapi/AikoBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.aiko.aikobackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AikoBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AikoBackendApiApplication.class, args);
+	}
+
+}
---
+++ aiko-backend-api/src/test/java/com/aiko/aikobackendapi/AikoBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiko.aikobackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AikoBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
