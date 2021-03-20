f41f096eb3fce1d771d1a9458d78871fc352acfb
Sadraque Nunes <sadraquenunesmartiniano@gmail.com>
2021-03-20 17:34:14 -0300

    implementacao do CRUD - Endpoint Paradas

--- aiko-backend-api/src/main/java/com/aiko/aikobackendapi/resources/ParadaResource.java
+++ aiko-backend-api/src/main/java/com/aiko/aikobackendapi/resources/ParadaResource.java
@@ -1,11 +1,62 @@
 package com.aiko.aikobackendapi.resources;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.aiko.aikobackendapi.domain.Parada;
+import com.aiko.aikobackendapi.services.ParadaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/paradas")
 public class ParadaResource {
+    @Autowired
+    private ParadaService paradaService;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> get(@PathVariable long id) {
+        Parada parada =  paradaService.buscar(id);
+
+        return (parada == null ? ResponseEntity.notFound().build() : ResponseEntity.ok().body(parada));
+    }
+
+    @RequestMapping(value = "")
+    public ResponseEntity<?> getAll() {
+        List<Parada> paradas =  paradaService.listar();
+
+        return (paradas == null) ? ResponseEntity.notFound().build() : ResponseEntity.ok().body(paradas);
+    }
+
+    @PostMapping(value = "")
+    public ResponseEntity<?> post(@RequestBody Parada parada) {
+        paradaService.adicionar(parada);
+
+        return ResponseEntity.created(URI.create("/veiculos/" +parada.getId())).body(parada);
+    }
+
+    @PutMapping(value = "")
+    public ResponseEntity<?> put(@RequestBody Parada parada) {
+        paradaService.atualizar(parada);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "")
+    public ResponseEntity<?> delete(@RequestBody Parada parada) {
+        paradaService.deletar(parada);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable long id) {
+        paradaService.deletar(id);
+
+        return ResponseEntity.ok().build();
+    }
+
 
 
 }
--- aiko-backend-api/src/main/java/com/aiko/aikobackendapi/services/ParadaService.java
+++ aiko-backend-api/src/main/java/com/aiko/aikobackendapi/services/ParadaService.java
@@ -1,12 +1,51 @@
 package com.aiko.aikobackendapi.services;
 
+import com.aiko.aikobackendapi.domain.Parada;
 import com.aiko.aikobackendapi.repositories.ParadaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ParadaService {
 
     @Autowired
     private ParadaRepository paradaRepository;
+
+    public Parada buscar(long id) {
+        Optional<Parada> parada = paradaRepository.findById(id);
+
+        return parada.orElse(null);
+    }
+
+    public List<Parada> listar() {
+        List<Parada> paradas = paradaRepository.findAll();
+
+        return (paradas.isEmpty()) ? null : paradas;
+    }
+
+    public void adicionar(Parada parada) {
+        paradaRepository.saveAll(Arrays.asList(parada));
+
+    }
+
+    public void atualizar(Parada parada) {
+        paradaRepository.saveAll(Arrays.asList(parada));
+
+    }
+
+    public void deletar(Parada parada) {
+
+        paradaRepository.delete(parada);
+
+    }
+
+    public void deletar(long id) {
+
+        paradaRepository.deleteById(id);
+
+    }
 }
