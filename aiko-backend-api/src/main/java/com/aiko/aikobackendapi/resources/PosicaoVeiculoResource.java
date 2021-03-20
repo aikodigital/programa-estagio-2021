7c6c6eb3c90279db076b59bf63534b81bc11544d
Sadraque Nunes <sadraquenunesmartiniano@gmail.com>
2021-03-20 17:58:08 -0300

    implementacao do CRUD - Endpoint PosicaoVeiculos

--- aiko-backend-api/src/main/java/com/aiko/aikobackendapi/resources/PosicaoVeiculoResource.java
+++ aiko-backend-api/src/main/java/com/aiko/aikobackendapi/resources/PosicaoVeiculoResource.java
@@ -1,11 +1,61 @@
 package com.aiko.aikobackendapi.resources;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.aiko.aikobackendapi.domain.PosicaoVeiculo;
+import com.aiko.aikobackendapi.services.PosicaoVeiculoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posicaoveiculos")
 public class PosicaoVeiculoResource {
 
+    @Autowired
+    private PosicaoVeiculoService posicaoVeiculoService;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> get(@PathVariable long id) {
+        PosicaoVeiculo posicaoVeiculo = posicaoVeiculoService.buscar(id);
+
+        return (posicaoVeiculo == null ? ResponseEntity.notFound().build() : ResponseEntity.ok().body(posicaoVeiculo));
+    }
+
+    @RequestMapping(value = "")
+    public ResponseEntity<?> getAll() {
+        List<PosicaoVeiculo> posicaoVeiculos = posicaoVeiculoService.listar();
+
+        return (posicaoVeiculos == null) ? ResponseEntity.notFound().build() : ResponseEntity.ok().body(posicaoVeiculos);
+    }
+
+    @PostMapping(value = "")
+    public ResponseEntity<?> post(@RequestBody PosicaoVeiculo posicaoVeiculo) {
+        posicaoVeiculoService.adicionar(posicaoVeiculo);
+
+        return ResponseEntity.created(URI.create("/posicaoveiculos/" +posicaoVeiculo.getId())).body(posicaoVeiculo);
+    }
+
+    @PutMapping(value = "")
+    public ResponseEntity<?> put(@RequestBody PosicaoVeiculo posicaoVeiculo) {
+        posicaoVeiculoService.atualizar(posicaoVeiculo);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "")
+    public ResponseEntity<?> delete(@RequestBody PosicaoVeiculo posicaoVeiculo) {
+        posicaoVeiculoService.deletar(posicaoVeiculo);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable long id) {
+        posicaoVeiculoService.deletar(id);
+
+        return ResponseEntity.ok().build();
+    }
 
 }
--- aiko-backend-api/src/main/java/com/aiko/aikobackendapi/services/PosicaoVeiculoService.java
+++ aiko-backend-api/src/main/java/com/aiko/aikobackendapi/services/PosicaoVeiculoService.java
@@ -1,12 +1,52 @@
 package com.aiko.aikobackendapi.services;
 
+import com.aiko.aikobackendapi.domain.PosicaoVeiculo;
+import com.aiko.aikobackendapi.domain.Veiculo;
 import com.aiko.aikobackendapi.repositories.PosicaoVeiculoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PosicaoVeiculoService {
 
     @Autowired
     private PosicaoVeiculoRepository posicaoVeiculoRepository;
+
+    public PosicaoVeiculo buscar(long id) {
+        Optional<PosicaoVeiculo> posicaoVeiculo = posicaoVeiculoRepository.findById(id);
+
+        return posicaoVeiculo.orElse(null);
+    }
+
+    public List<PosicaoVeiculo> listar() {
+        List<PosicaoVeiculo> posicaoVeiculos = posicaoVeiculoRepository.findAll();
+
+        return (posicaoVeiculos.isEmpty()) ? null : posicaoVeiculos;
+    }
+
+    public void adicionar(PosicaoVeiculo posicaoVeiculo) {
+        posicaoVeiculoRepository.saveAll(Arrays.asList(posicaoVeiculo));
+
+    }
+
+    public void atualizar(PosicaoVeiculo posicaoVeiculo) {
+        posicaoVeiculoRepository.saveAll(Arrays.asList(posicaoVeiculo));
+
+    }
+
+    public void deletar(PosicaoVeiculo posicaoVeiculo) {
+
+        posicaoVeiculoRepository.delete(posicaoVeiculo);
+
+    }
+
+    public void deletar(long id) {
+
+        posicaoVeiculoRepository.deleteById(id);
+
+    }
 }
