4cbafdb8f0b00c3ba2ff4f7a6a2e969ee876178f
Sadraque Nunes <sadraquenunesmartiniano@gmail.com>
2021-03-20 21:09:26 -0300

    implementacao do CRUD - Endpoint Linhas

--- aiko-backend-api/src/main/java/com/aiko/aikobackendapi/resources/LinhaResource.java
+++ aiko-backend-api/src/main/java/com/aiko/aikobackendapi/resources/LinhaResource.java
@@ -1,11 +1,61 @@
 package com.aiko.aikobackendapi.resources;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.aiko.aikobackendapi.domain.Linha;
+import com.aiko.aikobackendapi.services.LinhaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/linhas")
 public class LinhaResource {
 
+    @Autowired
+    private LinhaService linhaService;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> get(@PathVariable long id) {
+        Linha linha = linhaService.buscar(id);
+
+        return (linha == null ? ResponseEntity.notFound().build() : ResponseEntity.ok().body(linha));
+    }
+
+    @RequestMapping(value = "")
+    public ResponseEntity<?> getAll() {
+        List<Linha> linhas = linhaService.listar();
+
+        return (linhas == null) ? ResponseEntity.notFound().build() : ResponseEntity.ok().body(linhas);
+    }
+
+    @PostMapping(value = "")
+    public ResponseEntity<?> post(@RequestBody Linha linha) {
+        linhaService.adicionar(linha);
+
+        return ResponseEntity.created(URI.create("/linhas/" +linha.getId())).body(linha);
+    }
+
+    @PutMapping(value = "")
+    public ResponseEntity<?> put(@RequestBody Linha linha) {
+        linhaService.atualizar(linha);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "")
+    public ResponseEntity<?> delete(@RequestBody Linha linha) {
+        linhaService.deletar(linha);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable long id) {
+        linhaService.deletar(id);
+
+        return ResponseEntity.ok().build();
+    }
 
 }
--- aiko-backend-api/src/main/java/com/aiko/aikobackendapi/services/LinhaService.java
+++ aiko-backend-api/src/main/java/com/aiko/aikobackendapi/services/LinhaService.java
@@ -1,12 +1,51 @@
 package com.aiko.aikobackendapi.services;
 
+import com.aiko.aikobackendapi.domain.Linha;
 import com.aiko.aikobackendapi.repositories.LinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LinhaService {
 
     @Autowired
     private LinhaRepository linhaRepository;
+
+    public Linha buscar(long id) {
+        Optional<Linha> linha = linhaRepository.findById(id);
+
+        return linha.orElse(null);
+    }
+
+    public List<Linha> listar() {
+        List<Linha> linhas = linhaRepository.findAll();
+
+        return (linhas.isEmpty()) ? null : linhas;
+    }
+
+    public void adicionar(Linha linha) {
+        linhaRepository.saveAll(Arrays.asList(linha));
+
+    }
+
+    public void atualizar(Linha linha) {
+        linhaRepository.saveAll(Arrays.asList(linha));
+
+    }
+
+    public void deletar(Linha linha) {
+
+        linhaRepository.delete(linha);
+
+    }
+
+    public void deletar(long id) {
+
+        linhaRepository.deleteById(id);
+
+    }
 }
