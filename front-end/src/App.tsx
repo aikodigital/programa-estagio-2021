451b04da6294695bf315e9ae4c1f3c82e5bd877c
Giuulob89 <giulialobo89@gmail.com>
2021-04-12 21:28:50 -0300

    Modifying some details in the page

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,13 +3,13 @@ import busmarkerImg from './images/Logo.png';
 import lupaImg from './images/loupe 1.png';
 import mapmarkerImg from './images/location.png';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { Accordion, Card, InputGroup, FormControl } from 'react-bootstrap';
+import { Accordion, Card, InputGroup, FormControl, Button } from 'react-bootstrap';
 import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const mapIcon = Leaflet.icon ({
+const mapIcon = Leaflet.icon({
   iconUrl: mapmarkerImg,
   iconSize: [38, 48],
   iconAnchor: [29, 68],
@@ -31,15 +31,13 @@ function App() {
             </Accordion.Toggle>
               <Accordion.Collapse eventKey="0">
                 <Card.Body className="card-body">
-                  <h6>Digite o código da linha e localize os veículos</h6>
                   <InputGroup className="mb-3">
-                    <InputGroup.Prepend>
-                      <InputGroup.Text id="inputGroup-sizing-default"><img src={lupaImg} alt="Buscar" /></InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                      aria-label="Default"
-                      aria-describedby="inputGroup-sizing-default"
+                    <FormControl className = "form-control"
+                      placeholder="Digite o código da linha para os veículos"
                     />
+                    <InputGroup.Append>
+                      <Button variant="outline-secondary" id="inputGroup-sizing-default" ><img src={lupaImg} alt="Buscar" /></Button>
+                    </InputGroup.Append>
                   </InputGroup>
                   <br />
                 </Card.Body>
@@ -51,15 +49,13 @@ function App() {
             </Accordion.Toggle>
               <Accordion.Collapse eventKey="1">
                 <Card.Body className="card-body">
-                  <h6>Digite o código da linha e localize as paradas</h6>
                   <InputGroup className="mb-3">
-                    <InputGroup.Prepend>
-                      <InputGroup.Text id="inputGroup-sizing-default"><img src={lupaImg} alt="Buscar" /></InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                      aria-label="Default"
-                      aria-describedby="inputGroup-sizing-default"
+                    <FormControl className = "form-control"
+                      placeholder="Digite o código da linha para as paradas"
                     />
+                    <InputGroup.Append>
+                      <Button variant="outline-secondary" id="inputGroup-sizing-default" ><img src={lupaImg} alt="Buscar" /></Button>
+                    </InputGroup.Append>
                   </InputGroup>
                   <br />
                 </Card.Body>
@@ -71,15 +67,13 @@ function App() {
             </Accordion.Toggle>
               <Accordion.Collapse eventKey="2">
                 <Card.Body className="card-body">
-                  <h6>Digite o código da linha e vejas suas informações</h6>
                   <InputGroup className="mb-3">
-                    <InputGroup.Prepend>
-                      <InputGroup.Text id="inputGroup-sizing-default"><img src={lupaImg} alt="Buscar" /></InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                      aria-label="Default"
-                      aria-describedby="inputGroup-sizing-default"
+                    <FormControl className = "form-control"
+                      placeholder="Digite o código da linha"
                     />
+                    <InputGroup.Append>
+                      <Button variant="outline-secondary" id="inputGroup-sizing-default" ><img src={lupaImg} alt="Buscar" /></Button>
+                    </InputGroup.Append>
                   </InputGroup>
                   <br />
                 </Card.Body>
@@ -109,30 +103,26 @@ function App() {
               </Accordion.Toggle>
               <Accordion.Collapse eventKey="3">
                 <Card.Body className="card-body">
-                  <h6>Digite o código da linha</h6>
                   <InputGroup className="mb-3">
-                    <InputGroup.Prepend>
-                      <InputGroup.Text id="inputGroup-sizing-default"><img src={lupaImg} alt="Buscar" /></InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                      aria-label="Default"
-                      aria-describedby="inputGroup-sizing-default"
+                    <FormControl className = "form-control"
+                      placeholder="Digite o código da linha"
                     />
+                    <InputGroup.Append>
+                      <Button variant="outline-secondary" id="inputGroup-sizing-default" ><img src={lupaImg} alt="Buscar" /></Button>
+                    </InputGroup.Append>
                   </InputGroup>
                   <br />
                 </Card.Body>
               </Accordion.Collapse>
               <Accordion.Collapse eventKey="3">
                 <Card.Body className="card-body">
-                  <h6>Digite o nome da parada na qual se localiza</h6>
                   <InputGroup className="mb-3">
-                    <InputGroup.Prepend>
-                      <InputGroup.Text id="inputGroup-sizing-default"><img src={lupaImg} alt="Buscar" /></InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                      aria-label="Default"
-                      aria-describedby="inputGroup-sizing-default"
+                    <FormControl className = "form-control"
+                      placeholder="Digite o nome de sua parada atual"
                     />
+                    <InputGroup.Append>
+                      <Button variant="outline-secondary" id="inputGroup-sizing-default" ><img src={lupaImg} alt="Buscar" /></Button>
+                    </InputGroup.Append>
                   </InputGroup>
                   <br />
                 </Card.Body>
@@ -151,10 +141,10 @@ function App() {
           </Accordion>
         </div>
       </aside>
-      <MapContainer center = {[-23.6815315, -46.8754901]} zoom = {15} style = {{ width: '100%', height: '100%' }}>
+      <MapContainer center={[-23.6815315, -46.8754901]} zoom={15} style={{ width: '100%', height: '100%' }}>
         <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
-        <Marker icon = {mapIcon} position={[-23.6815315, -46.8754901]}>
-          <Popup closeButton = {false}>
+        <Marker icon={mapIcon} position={[-23.6815315, -46.8754901]}>
+          <Popup closeButton={false}>
             Localização Atual
           </Popup>
         </Marker>
