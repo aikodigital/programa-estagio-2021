17b1e9d3b38bb48ab99e0b17947dd9faaf97ff8f
Murilo Gomes <murilo.goomes@hotmail.com>
2021-04-13 10:29:13 -0300

    feat: initialize project with typescript, eslint and express

---
+++ projeto/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.json({
+    message: 'Hello World',
+  });
+});
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333');
+});
+
