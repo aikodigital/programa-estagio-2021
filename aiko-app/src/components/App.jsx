d0335cce0d9620f5d703e3a55c776d13eab5bf2b
Brendo Katagi <brendo.katagi@gmail.com>
2021-04-13 22:58:41 -0300

    feat: insert map with vehicle position

--- aiko-app/src/components/App.jsx
+++ aiko-app/src/components/App.jsx
@@ -5,17 +5,9 @@ import Header from "./Header";
 
 require('dotenv').config()
 
-
-
 function Map() {
   const [selectedBus, setSelectedBus] = useState(null);
-  const image = {
-    url:
-      "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-    size: new google.maps.Size(20, 32),
-    origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(0, 32),
-  };
+
   return (<GoogleMap 
     defaultZoom = {10} 
     defaultCenter={{lat:-23.55556, lng:-46.61984}}>
@@ -59,6 +51,7 @@ function Map() {
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function App() {
+
   return( 
     <div>
       <Header />
