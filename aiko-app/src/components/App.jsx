7f7881cb0767b6c848358f73fa22228e591f3a9e
Brendo Katagi <brendo.katagi@gmail.com>
2021-04-16 00:13:36 -0300

    feat: define map and form layout

--- aiko-app/src/components/App.jsx
+++ aiko-app/src/components/App.jsx
@@ -1,66 +1,32 @@
-import React, {useState} from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps"
-import * as busPos from "../data/posVeiculo.json";
+import React from 'react';
+import { withScriptjs, withGoogleMap} from "react-google-maps"
 import Header from "./Header";
+//import stopPosition from './paradaPos';
+import Map from './Map';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FormData from "./FormData";
 
 require('dotenv').config()
 
-function Map() {
-  const [selectedBus, setSelectedBus] = useState(null);
-
-  return (<GoogleMap 
-    defaultZoom = {10} 
-    defaultCenter={{lat:-23.55556, lng:-46.61984}}>
-    {busPos.l.map(bus => (
-
-      <Marker 
-        key={bus.cl} 
-        position={{
-          lat: bus.vs[0].py,
-          lng: bus.vs[0].px
-        }}
-        onClick={() => {
-          setSelectedBus(bus);
-        }}
-      />
-      
-    ))}
-
-    {selectedBus && (
-      <InfoWindow 
-          position={{
-            lat: selectedBus.vs[0].py,
-            lng: selectedBus.vs[0].px
-        }}
-        onCloseClick={() => {
-          setSelectedBus(null);
-        }}
-      > 
-        <div>
-          <h3>{selectedBus.c}</h3>
-          <p>Origem: {selectedBus.lt0}</p>
-          <p>Destino: {selectedBus.lt1}</p>
-        </div>
-      </InfoWindow>
-    )}
-    </GoogleMap>
-  );
-
-} 
+//const posData = createJson();
 
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function App() {
-
   return( 
     <div>
       <Header />
-      <div className = "map-div">
-        <WrappedMap googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
-          loadingElement = {<div style= {{height: "100%"}} />}
-          containerElement = {<div style= {{height: "100%"}} />}
-          mapElement = {<div style= {{height: "100%"}} />}
-        />
+      <div className = "dashboard-div">
+        <div className = "map-div">
+          <WrappedMap googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+            loadingElement = {<div style= {{height: "100%"}} />}
+            containerElement = {<div style= {{height: "100%"}} />}
+            mapElement = {<div style= {{height: "100%"}} />}
+          />
+        </div>
+        <div className="filter-div">
+          <FormData />
+        </div>
       </div>
     </div>
   ); 
