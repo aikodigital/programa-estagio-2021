ce0e021dbae209fdbcd2e84845010378896e3d69
berrytern <berrytern@gmail.com>
2021-04-16 17:35:29 -0300

    added dockerfile and docker-compose

--- src/database/connection/settings.js
+++ src/database/connection/settings.js
@@ -1,4 +1,4 @@
 const pg_conn = 'postgres://' + (process.env.USER_DB || 'postgres') + ':' + (process.env.PASSWORD_DB || 'postgres') + '@' + (process.env.DB_DOMAIN || 'localhost') + ':5432/postgres';
 const dbName = process.env.DATABASE_NAME || 'aiko';
-const db_url = 'postgres://postgres:postgres@localhost:5432/' + dbName;
+const db_url = 'postgres://' + (process.env.USER_DB || 'postgres') + ':' + (process.env.PASSWORD_DB || 'postgres') + '@' +(process.env.DB_DOMAIN || 'localhost') + ':5432/' + dbName;
 module.exports = { db_url, dbName, pg_conn };
