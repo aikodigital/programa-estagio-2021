8b5e65f112b63f6f04e299b4d6d2ce30d5311238
berrytern <berrytern@gmail.com>
2021-04-16 19:36:52 -0300

    testing creation before run

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const createdb = require('./src/database/connection/database')();
+const createdb = require('./src/database/connection/database');
+(async()=>{
+	await createdb();
+	const app = require('./src/app.js');
+	const Port = process.env.PORT || 3000;
+	app.listen(Port,()=>{console.log('running on Port: '+Port);});
 
-const app = require('./src/app.js');
-const Port = process.env.PORT || 3000;
-app.listen(Port,()=>{console.log('running on Port: '+Port);});
+})();
 
