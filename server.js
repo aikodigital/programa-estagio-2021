1462626f919ac2a5b5e9943492312c193e88b127
berrytern <berrytern@gmail.com>
2021-04-16 19:32:10 -0300

    testing creation before run

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const createdb = require('./src/database/connection/database')();
-setTimeout(()=>{
-	const app = require('./src/app.js');
-	const Port = process.env.PORT || 3000;
-	app.listen(Port,()=>{console.log('running on Port: '+Port);});}
-, 150);
+
+const app = require('./src/app.js');
+const Port = process.env.PORT || 3000;
+app.listen(Port,()=>{console.log('running on Port: '+Port);});
+
--- src/database/connection/database.js
+++ src/database/connection/database.js
@@ -11,7 +11,7 @@ const create = async () => {
 		}
 	});
 	// create the db and ignore any errors, for example if it already exists.
-	return client.query('CREATE DATABASE ' + dbName, function () {
+	return await client.query('CREATE DATABASE ' + dbName, function () {
 		//db should exist now, initialize Sequelize
 		console.log(`create database "${ dbName }" if not exists`);
 		client.end(); // close the connection
