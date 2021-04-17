07aa05ba5c046f35c811964e213a6babfafda163
Brendo Katagi <brendo.katagi@gmail.com>
2021-04-17 02:06:34 -0300

    feat: get data from form to set map variables

--- aiko-app/src/components/FormData.jsx
+++ aiko-app/src/components/FormData.jsx
@@ -1,32 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import * as linhas from "../data/linhas.json";
+import { UseMap } from "./mapContext";
+import paradaJson from "./paradaJson";
+import posJson from "./posJson";
 
 function FormData(){
+    const { setMapData } = UseMap();
+    const [search, setSearch] = useState({
+        numLinha: null,
+        option: "Ônibus"
+    })
 
-    function searchParams(event){
-        event.preventDefault();
-        console.log(event.target.elements.controlSelect2.value);
-    }
+    useEffect(() => {
+        let option = search.option;
+        let termo = search.termo;
 
-    return(<Form onSubmit={(e) => searchParams(e)}>
+        if(option === "Paradas"){
+
+            console.log("Clicou em estações");
+
+            const fetchedData = async () => {
+                const fetchData = await paradaJson(termo);
+                setMapData({
+                    info : termo,
+                    busPos : fetchData,
+                    searchData : {
+                        icon :  'https://img.icons8.com/material/452/stop-sign-2.png',
+                        info1 : 'Código:',
+                        info2 : 'Nome:',
+                        info3 : 'Localização:'
+                    }
+                })
+            }
+
+            fetchedData();
+
+        }else{
+            console.log("Clicou em onibus");
+
+            const fetchedBus = async () => {
+                const fetchBus = await posJson();
+                setMapData({
+                    info : termo,
+                    busPos : fetchBus,
+                    searchData : {
+                        icon :  'https://image.freepik.com/free-icon/bus_318-2038.jpg',
+                        info1 : 'Ônibus:',
+                        info2 : 'Origem:',
+                        info3 : 'Destino'
+                    }
+                })   
+            }
+            fetchedBus();
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [search]);
+
+    return(<Form onSubmit={(e) => (
+        e.preventDefault(),
+        setSearch(
+            {termo: e.target.elements.controlSelect2.value,
+             option: e.target.elements.controlSelect1.value
+            }
+        ))}>
             <Form.Group controlId="controlSelect1">
-                <Form.Label>Selecione a linha:</Form.Label>
-                <Form.Control as="select">
-                {linhas.default.map(item => (
-                    <option key={item.cl} value={item.cl}>{item.cl}</option>
-                ))
-                }
-                </Form.Control>
+                <Form.Label>Selecione opção de busca:</Form.Label>
+                    <Form.Control as="select">
+                        <option>Ônibus</option>
+                        <option>Paradas</option>
+                    </Form.Control>
             </Form.Group>
             <Form.Group controlId="controlSelect2">
-                <Form.Label>Example multiple select</Form.Label>
-                <Form.Control as="select">
-                <option>Ônibus</option>
-                <option>Estações</option>
+                <Form.Label>Digite o endereço da parada:</Form.Label>
+                    <Form.Control type="text">
                 </Form.Control>
             </Form.Group>
-
             <Button variant="primary" type="submit">
                 Submit
             </Button>
