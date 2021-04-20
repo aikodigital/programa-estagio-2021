9f46993ce28105030f800e37d0879eca4f0fb6e2
AlessandraRomero <alessandraromeiro098@gmail.com>
2021-04-19 20:55:23 -0400

    Cria servidor

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,9 @@
-import express, { NextFunction, Request, response, Response } from 'express';
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
-import AppError from '@shared/errors/AppError';
+import AppError from './errors/AppError';
+import '../typeorm';
 
 const app = express();
 
