9db26488543e53c0ef565ecfb6ce78b0af157863
AlessandraRomero <alessandraromeiro098@gmail.com>
2021-04-21 11:22:06 -0400

    Implementação do controller de parada

---
+++ src/modules/paradas/controllers/ParadasController.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from 'express';
+import CreateParadaService from '../services/CreateParadaService';
+import ListParadaService from '../services/ListParadaService';
+import ShowParadaService from '../services/ShowParadaService';
+import UpdateParadaService from '../services/UpdateParadaService';
+import DeleteParadaService from '../services/DeleteParadaService';
+
+export default class ParadasController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listParadas = new ListParadaService();
+    const paradas = await listParadas.execute();
+
+    return response.json(paradas);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showParada = new ShowParadaService();
+
+    const parada = await showParada.execute({ id });
+
+    return response.json(parada);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, latitude, longitude } = request.body;
+
+    const createParada = new CreateParadaService();
+
+    const parada = await createParada.execute({
+      name,
+      latitude,
+      longitude,
+    });
+
+    return response.json(parada);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { name, latitude, longitude } = request.body;
+    const { id } = request.params;
+
+    const updateParada = new UpdateParadaService();
+
+    const parada = await updateParada.execute({
+      id,
+      name,
+      latitude,
+      longitude,
+    });
+
+    return response.json(parada);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteParada = new DeleteParadaService();
+
+    await deleteParada.execute({ id });
+
+    return response.json([]);
+  }
+}
