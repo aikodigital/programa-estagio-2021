98f797f964bc041a457945b88bba3e8594326ee6
AlessandraRomero <alessandraromeiro098@gmail.com>
2021-04-21 11:10:44 -0400

    Implementaçao do serviço listar linha por id

---
+++ src/modules/linhas/services/ShowLinhaService.ts
@@ -0,0 +1,24 @@
+import AppError from '@shared/http/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import 'express-async-errors';
+import Linha from '../typeorm/entities/Linha';
+import { LinhaRepository } from '../typeorm/repositories/LinhaRepository';
+interface IRequest {
+  id: string;
+}
+
+class ShowLinhaService {
+  public async execute({ id }: IRequest): Promise<Linha | undefined> {
+    const linhaRepository = getCustomRepository(LinhaRepository);
+
+    const linha = await linhaRepository.findOne(id);
+
+    if (!linha) {
+      throw new AppError('Linha não encontrada');
+    }
+
+    return linha;
+  }
+}
+
+export default ShowLinhaService;
