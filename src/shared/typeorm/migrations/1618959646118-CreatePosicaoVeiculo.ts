4bb5d7e270bef1011d79afa9a00e2bf596b0babf
AlessandraRomero <alessandraromeiro098@gmail.com>
2021-04-21 18:17:58 -0400

    migration posicaoVeiculo

---
+++ src/shared/typeorm/migrations/1618959646118-CreatePosicaoVeiculo.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreatePosicaoVeiculo1618959646118 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'posicaoVeiculos',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'latitude',
+            type: 'decimal',
+          },
+          {
+            name: 'longitude',
+            type: 'decimal',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'veiculoId',
+            referencedTableName: 'veiculos',
+            referencedColumnNames: ['id'],
+            columnNames: ['id'],
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('posicaoVeiculos');
+  }
+}
