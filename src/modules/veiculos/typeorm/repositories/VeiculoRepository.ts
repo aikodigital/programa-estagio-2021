621ee77b41336c98b4dc24c4c5ab3de17a2cf50c
AlessandraRomero <alessandraromeiro098@gmail.com>
2021-04-25 14:26:52 -0400

    Retirando código que não foi usado

--- src/modules/veiculos/typeorm/repositories/VeiculoRepository.ts
+++ src/modules/veiculos/typeorm/repositories/VeiculoRepository.ts
@@ -3,22 +3,6 @@ import Veiculo from '../entities/Veiculo';
 
 @EntityRepository(Veiculo)
 export class VeiculoRepository extends Repository<Veiculo> {
-  public async findByLinha(linhaId: string): Promise<Veiculo | undefined> {
-    const veiculo = this.findOne({
-      where: {
-        linhaId,
-      },
-    });
-    return veiculo;
-  }
-  public async findById(veiculoId: string): Promise<Veiculo | undefined> {
-    const veiculo = this.findOne({
-      where: {
-        veiculoId,
-      },
-    });
-    return veiculo;
-  }
   public async findByName(name: string): Promise<Veiculo | undefined> {
     const veiculo = this.findOne({
       where: {
