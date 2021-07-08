67e6f20c4e0d50f643cedbd9e63914e374302813
Carlos Henrique Lima Cicoti <carlosMvSep3@hotmail.com>
2021-07-08 09:18:42 -0300

    Criando a classe Parada

---
+++ Models/Parada.cs
@@ -0,0 +1,11 @@
+namespace programa_estagio_2021.Models
+{
+    public class Parada
+    {
+        public long Id {get; set;}
+        public string Name {get; set;}
+        public double Latitude {get; set;}
+        public double Longitude {get; set;}
+        
+    }
+}
