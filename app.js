3fff9bc98cac0a7f8a112302a504b0098febd8b8
MurilloSSJ <murillostore@gmail.com>
2021-07-12 16:51:39 -0300

    configurando rotas posição do veículo

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const vehiclePositionRoutes = require('./rotas/vehiclePosition')
 
-
+//Listando rotas
+app.use('/vehicle-position',vehiclePositionRoutes)
 
 //Colocando server no ar
 app.listen(PORT,()=>{
